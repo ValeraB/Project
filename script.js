d50040eea8e6860b678b9194adcd225e70f0c62b
ValeraB <90112682+ValeraB@users.noreply.github.com>
2021-09-05 00:47:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 "use strict";
 
+const answer1 = +prompt("Enter first number", "");
+const answer2 = +prompt("Enter second number", "");
 
-
-const r = 1;
-
-alert(r);
+if (answer1 > answer2) {
+  alert(`${answer1} ,is bigger than ${answer2}`);
+} else if (answer1 < answer2) {
+  alert(`${answer2}, is bigger than ${answer1}`);
+}
